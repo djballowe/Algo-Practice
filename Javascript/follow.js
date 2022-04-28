8c4c20b977696d383fe772218dd1bf0528210ae2
djballowe <djballowe@gmail.com>
2022-04-28 13:43:06 -0700

    finished follow.js

---
+++ Javascript/follow.js
@@ -0,0 +1,10 @@
+const firstName = 'David';
+const lastName = 'Ballowe';
+const curYear = 2022;
+const bYear = 1996;
+
+const age = curYear - bYear;
+
+greeting = `Hi, my name is ${firstName} ${lastName} and I am ${age} years old.`
+
+console.log(greeting);
