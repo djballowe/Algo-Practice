4098f92b791d972be527ce7c67b8551d7e24715c
djballowe <djballowe@gmail.com>
2022-04-29 13:30:02 -0700

    finished follow

--- Javascript/follow.js
+++ Javascript/follow.js
@@ -1,33 +1,43 @@
-// var commonChars = function(words) {
-    //bbc = []
-    //let key = []
-    //key.push(words[0]);
-    //stinky = key.join('');
-    //words.shift();
-    //compare = words.join('');
-    
-    //for (let i = 0, len = stinky.length; i < len; i++) {
-        //if(compare.indexOf(stinky[i]) !=1) {
-            //sim = stinky.charAt(i);
-            //bbc.push(sim);
-        //}
-    //}
-    //return bbc;
-//};
-
 var commonChars = function(words) {
-    let fin = [];
-    let key = [];
-   key.push(words[0]);
-   for (let i = 0; i < words; i++) {
-       if (key.indexOf(words[i]) !=1) {
-           sim = words.charAt(i);
-           fin.push(sim);
-       }
-       
-   }
-   return fin;
-};
+    let matches = [];
+    let map = {};
+    
+        for(let val of words[0]){
+            if (map[val]) {
+                map[val] = map[val] + 1; //if there is more than one letter +1 to value
+            } else {
+                map[val] = 1;   // else value = 1  
+        }
+    }
+        for (let i = 1; i < words.length; i++) {
+            compare = {};
+            for(let val of words[i]) { // same thing just with object compare
+                if (compare[val]) {
+                    compare[val] = compare[val] + 1;
+                } else {
+                    compare[val] = 1 //else compare value = 1
+                }
+            }
+            for (const [key, value] of Object.entries(map)) {
+                if(compare[key]) {                  //if compare has the same key as map 
+                    if (value > compare[key]) {     //if value of map is > value @ compare[key]
+                        map[key] = compare[key]     //map[key] = compare[key]
+                    } else {
+                        map[key] = value           //else map[key] value stays the same
+                    }
+                } else {
+                    delete map[key];                // no match? delete map[key]
+                }   
+            }
+        }
+        for (const [key, value] of Object.entries(map)) {
+            for (let i = 0; i < value; i++) {
+                matches.push(key);
+            }
+        }
+        return matches;
+    }
+    
 
 
 
@@ -40,3 +50,5 @@ console.log(commonChars(["bella", "label", "roller"]));
 // find a way to iterate over the first word and then compare those letters to the other two words
 // assign words[0] to a value and use that to compare as you only need that one separated.
 // use word[0] as the key
+// compare the first word to the key and populate a new array with only the matches 
+// compare the next word with the matches and populate the array with only matches
