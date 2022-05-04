d619a401c13d4ecc1bf4bb835888140f769a1980
djballowe <djballowe@gmail.com>
2022-05-04 07:34:14 -0700

    finished vowel code wars problem

--- Javascript/spinWords.js
+++
@@ -1,8 +0,0 @@
-function spinWords(string){
-  let split = string.split('');
-  let reverse = split.reverse();
-  let join = reverse.join('');
-  return join;
-}
-
-console.log(spinWords('another one bites the dust'));
