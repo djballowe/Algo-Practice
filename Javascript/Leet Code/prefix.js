9a40ee93fa3b8ef00afc954f2608727c332efc26
djballowe <djballowe@gmail.com>
2022-05-06 19:40:01 -0700

    changes

---
+++ Javascript/Leet Code/prefix.js
@@ -0,0 +1,12 @@
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+ var longestCommonPrefix = function(strs) {
+    
+};
+
+console.log(['flower', 'flow', 'flight']);
+ // console.log(["dog","racecar","car"]);
+
+ // 
