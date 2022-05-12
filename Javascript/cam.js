555a1bd801a8f8a7b116d2297e79409924369310
djballowe <djballowe@gmail.com>
2022-05-11 20:53:33 -0700

    new

--- Javascript/cam.js
+++ Javascript/cam.js
@@ -1,12 +1,16 @@
 function toCamelCase(str){
-    let ans = ''
+    let ans = [];
     for(let i = 0; i < str.length; i++) {
         if(str[i] === '-' || str[i] === '_') {
+            a = str[i+1].toUpperCase();
+            ans.push(a);
             
-            str[i-1] = str[i-1].toUpperCase();
+        } else {
+            ans.push(str[i]);
         }
-     
+        
     }
+    return ans;
 }
 
 console.log(toCamelCase("the_stealth_warrior"));
