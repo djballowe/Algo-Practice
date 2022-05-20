7e2254da39be2ce2f84df799964a687c7696dd6b
djballowe <djballowe@gmail.com>
2022-05-20 09:44:19 -0700

    binary search

--- Javascript/Leet Code/sqrt.js
+++ Javascript/Leet Code/sqrt.js
@@ -6,29 +6,30 @@
     let count2 = 0;
     let count3 = 0;
     let count5 = 0;
-    while (x !== 1) {
+    while (x != 1) {
         if (x % 2 === 0) {
-            x/2
+            x = x / 2
             count2++;
         } else if (x % 3 === 0) {
-            x/3
+            x = x / 3
             count3++;
         } else if (x % 5 === 0) {
-            x/5
+            x = x / 5
             count5++;
         }
     }
     if (count2 !== 0) {
-        two = 2*count2;
+        two = count2 / 2;
     }
     if (count3 !== 0) {
-        three = 3*count3;
+        three = count3 / 2;
+        factor3 = 3 * three;
     }
-    ans = two * three;
+    ans = two;
     return ans;
     
 };
 
-console.log(mySqrt(144))
+console.log(mySqrt(8));
 
 // square root (x) Leet Code 5/20
