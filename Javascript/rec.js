73ed5c2ffeb0f489ecca0da28c5483ec36255a97
djballowe <djballowe@gmail.com>
2022-06-07 16:23:38 -0700

    recursion

--- Javascript/rec.js
+++ Javascript/rec.js
@@ -1,7 +1,10 @@
 function digital_root(n) {
-    for (let i = 0; i < n; i++) {
-        sum 
+    let arr = Array.from(String(n), Number);
+    let newNum = 0;
+    for (let i = 0; i < arr.length; i++) {
+       newNum += arr[i];
     }
+    return newNum;
 }
 
 console.log(digital_root(456))
