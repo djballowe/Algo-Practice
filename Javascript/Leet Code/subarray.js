aa9830864edfc6395d98210a419e21f0e89b1b3c
David Ballowe <djballowe@gmail.com>
2022-06-25 21:13:01 -0700

    redoing subarray

--- Javascript/Leet Code/subarray.js
+++ Javascript/Leet Code/subarray.js
@@ -3,73 +3,38 @@
  * @return {number}
  */
  var maxSubArray = function(nums) {
-    let biggest = nums[0];
-    let smallest = nums[0];
-    let sum = 0;
+    let sum;
+    let startIndex;
+    let endIndex;
+    let arrLength = endIndex - startIndex;
     for (let i = 0; i < nums.length; i++) {
-        if(nums[i] > biggest) {
-            biggest = nums[i];
+        if (nums[i] > 0) {
+            start = nums[i];
+            startIndex = i;
+            break;
+        }
+        for (let j = nums.length - 1; j > 0; j++) {
+            if (nums[j] > 0) {
+                end = nums[j];
+                endIndex = j;
+                break;
+            } 
         } 
-        if (nums[i] < smallest) {
-            smallest = nums[i];
-        }
-        sum += nums[i];
     }
-    if (biggest < 0) {
-        return biggest;
+    for (let e = startIndex; e < arrLength; e++) {
+        sum = nums[e] + nums[e + 1];
+        return end;
     }
-    if(smallest > 0) {
-        return sum;
-    }
-
-    let start = 0;
-    let end = nums.length - 1;
-    let startPair = start;
-    let endPair = end;
-    let biggestSum = sum;
-    let startSum = nums[start];
-    let endSum = nums[end];
-    while (startPair < endPair) {
-        if(nums[start] < 0) {
-            start ++;
-            startPair = start;
-            sum -= nums[start];
-            biggestSum = sum;
-            startSum = nums[start];
-        }
-        if (nums[end] < 0) {
-            end --;
-            endPair = end;
-            sum -= nums[end];
-            biggestSum = sum;
-            endSum = nums[end];
-        }
-        if (nums[startPair] < 0 && nums[startPair + 1] >= 0) {
-           if (startSum < 0) {
-               start = startPair + 1;
-               startPair = start;
-               sum -= startSum;
-           }
-        } else {
-            startPair++;
-            startSum += nums[startPair];
-        }
-        if (nums[endPair] < 0 && nums[endPair - 1] >= 0) {
-            if (endSum < 0) {
-                end = endPair - 1;
-                endPair = end;
-            }
-         } else {
-            endPair--;
-            endSum += nums[endPair];
-            sum -= endSum;
-
-         } 
-    }
-    return sum;
-
+    return end;
 };
 
 console.log(maxSubArray([-2,1,-3,4,-1,2,1,-5,4]));
 
-// start and end of array must be +
+// 1, -3, 4, -1, 2, 1, -5, 100 
+// the first and last number have to be postive 
+// only test the in a row numbers starting and ending with a positive number
+
+
+
+
+
