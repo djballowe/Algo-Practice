c6ff5155fb187073f24528d8c3093823a0822b2c
David Ballowe <djballowe@gmail.com>
2022-06-29 20:58:02 -0700

    subarray

--- Javascript/Leet Code/subarray.js
+++ Javascript/Leet Code/subarray.js
@@ -2,39 +2,34 @@
  * @param {number[]} nums
  * @return {number}
  */
- var maxSubArray = function(nums) {
-    let sum;
-    let startIndex;
-    let endIndex;
-    let arrLength = endIndex - startIndex;
-    for (let i = 0; i < nums.length; i++) {
-        if (nums[i] > 0) {
-            start = nums[i];
-            startIndex = i;
-            break;
-        }
-        for (let j = nums.length - 1; j > 0; j++) {
-            if (nums[j] > 0) {
-                end = nums[j];
-                endIndex = j;
-                break;
-            } 
-        } 
+var maxSubArray = function (nums) {
+  let sum;
+  let startIndex;
+  let endIndex;
+  let arrLength = endIndex - startIndex;
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] > 0) {
+      start = nums[i];
+      startIndex = i;
+      break;
     }
-    for (let e = startIndex; e < arrLength; e++) {
-        sum = nums[e] + nums[e + 1];
-        return end;
+    for (let j = nums.length - 1; j > 0; j++) {
+      if (nums[j] > 0) {
+        end = nums[j];
+        endIndex = j;
+        break;
+      }
     }
+  }
+  for (let e = startIndex; e < arrLength; e++) {
+    sum = nums[e] + nums[e + 1];
     return end;
+  }
+  return end;
 };
 
-console.log(maxSubArray([-2,1,-3,4,-1,2,1,-5,4]));
+console.log(maxSubArray([-2, 1, -3, 4, -1, 2, 1, -5, 4]));
 
-// 1, -3, 4, -1, 2, 1, -5, 100 
-// the first and last number have to be postive 
+// 1, -3, 4, -1, 2, 1, -5, 100 do this first thing in the morning
+// the first and last number have to be postive
 // only test the in a row numbers starting and ending with a positive number
-
-
-
-
-
