26ffb3bebd756b4f0bca58de1e516b88234ca7ad
David Ballowe <djballowe@gmail.com>
2022-06-30 17:35:50 -0700

    max subarray

--- Javascript/Leet Code/subarray.js
+++ Javascript/Leet Code/subarray.js
@@ -3,33 +3,19 @@
  * @return {number}
  */
 var maxSubArray = function (nums) {
-  let sum;
-  let startIndex;
-  let endIndex;
-  let arrLength = endIndex - startIndex;
-  for (let i = 0; i < nums.length; i++) {
-    if (nums[i] > 0) {
-      start = nums[i];
-      startIndex = i;
-      break;
+  let maxSum = nums[0];
+  let sum = 0;
+  nums.forEach((n) => {
+    if (sum < 0) {
+      sum = 0;
     }
-    for (let j = nums.length - 1; j > 0; j++) {
-      if (nums[j] > 0) {
-        end = nums[j];
-        endIndex = j;
-        break;
-      }
-    }
-  }
-  for (let e = startIndex; e < arrLength; e++) {
-    sum = nums[e] + nums[e + 1];
-    return end;
-  }
-  return end;
+    sum += n;
+    maxSum = Math.max(maxSum, sum);
+  });
+  return maxSum;
 };
 
 console.log(maxSubArray([-2, 1, -3, 4, -1, 2, 1, -5, 4]));
 
-// 1, -3, 4, -1, 2, 1, -5, 100 do this first thing in the morning
 // the first and last number have to be postive
 // only test the in a row numbers starting and ending with a positive number
