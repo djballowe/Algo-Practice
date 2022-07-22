41148b3015098eded7d5f342330160f73c200465
David Ballowe <djballowe@gmail.com>
2022-07-21 21:29:01 -0700

    leet

--- Javascript/Leet Code/mergArray.js
+++ Javascript/Leet Code/mergArray.js
@@ -1,8 +1,16 @@
 const merge = function (nums1, m, nums2, n) {
-  for (let i = 0; i < nums2.; i++) {
+  for (let i = 0; i < nums2.length; i++) {
     nums1.push(nums2[i]);
   }
-  return nums1.sort();
+  nums1.sort();
+  let fakeNums = [];
+  for (let i = 0; i < nums1.length; i++) {
+    if (nums1[i] !== 0) {
+      fakeNums.push(nums1[i]);
+    }
+  }
+  nums1 = fakeNums;
+  return nums1;
 };
 
-console.log(merge([1, 2, 3, 0, 0, 0], 3, [2, 5, 6], 3));
+console.log(merge([1, 2, 3], 3, [2, 5, 6], 3));
